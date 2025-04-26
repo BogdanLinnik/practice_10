93a3a22f44b91f69a44021b824c40788ca6f26be
Bogdan Linnik <bogdan@geologie.com>
2025-04-26 19:57:19 +0300

    Try to enable code smell feature by using eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,52 @@
 module.exports = {
   env: {
-    commonjs: true,
-    es2021: true,
     node: true,
+    es2021: true,
+    jest: true
   },
-  extends: ['standard', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier'
+  ],
   parserOptions: {
     ecmaVersion: 12,
+    sourceType: 'module'
   },
-  rules: {},
-}
+  plugins: ['import'],
+  rules: {
+    // Code Smell Detection Rules
+    'complexity': ['error', { max: 15 }],
+    'max-lines-per-function': ['error', { max: 50 }],
+    'max-lines': ['error', { max: 500 }],
+    'max-params': ['error', { max: 5 }],
+    'max-depth': ['error', { max: 4 }],
+    'max-statements': ['error', { max: 50 }],
+    'no-duplicate-imports': 'error',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': 'warn',
+    'no-alert': 'error',
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+    'no-return-await': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-arrow-callback': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': ['error', {
+      'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+      'newlines-between': 'always',
+      'alphabetize': { 'order': 'asc' }
+    }]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js']
+      }
+    }
+  }
+};
